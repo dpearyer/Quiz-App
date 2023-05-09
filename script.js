00c965dff7d8bab36b7a1e0672d3391ce8f4de44
dpearyer <dpear1050@gmail.com>
2023-05-08 22:50:59 -0400

    javascript add

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Who is the bestest?",
+        answers:[
+            {test:"A Koala", correct:false},
+            {test:"Super Woman", correct:false},
+            {test:"You", correct:true},
+            {test:"Killer Shark", correct:false}
+        ]
+    }
+]
