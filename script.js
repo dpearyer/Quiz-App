9bd10d706fe38c13ff24508e3d893d4d42310549
dpearyer <dpear1050@gmail.com>
2023-05-23 12:15:06 -0400

    fixed correct answer bug

--- script.js
+++ script.js
@@ -53,6 +53,10 @@ function showQuestion(){
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
@@ -62,5 +66,23 @@ function resetState(){
         answerButton.removeChild(answerButton.firstChild);
     }
 }
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct==="true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display="block";
+}
+
 
 startQuiz();
